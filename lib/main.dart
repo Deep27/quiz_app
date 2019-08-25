6134925bf74a73fa30234917bcbe930f76be5ea5
Deep27 <geep2795@gmail.com>
2019-08-26 01:53:44 +0300

    Using StatefulWidget.

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,52 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(QuizApp());
 
-class QuizApp extends StatelessWidget {
+class QuizApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return QuizAppState();
+  }
+}
+
+class QuizAppState extends State<QuizApp> {
+  int questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<String> questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
+
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Quiz App'),
-        ),
-        body: Text('Quiz App'),
-      ),
+          appBar: AppBar(
+            title: Text('Quiz App'),
+          ),
+          body: Column(
+            children: <Widget>[
+              Text(questions[questionIndex]),
+              RaisedButton(
+                child: Text('Answer 1'),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text('Answer 2'),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text('Answer 2'),
+                onPressed: answerQuestion,
+              ),
+            ],
+          )),
     );
   }
 }
